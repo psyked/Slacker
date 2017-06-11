b68ef7f7e353e8e0e82ae8ae38f3e94e46246afa
James Ford <james.f@mmtdigital.co.uk>
2017-06-11 22:57:12 +0100

    Make neat dashboard ui with blessed

--- app.js
+++ app.js
@@ -9,17 +9,29 @@ const STATES = { ACTIVE: 'active', AWAY: 'away' }
 let users
 
 slack.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
-    console.log('Welcome to Slack. You are logged in as @%s of %s'.blue, rtmStartData.self.name, rtmStartData.team.name)
+    log.log(`Welcome to Slack. You are logged in as @${rtmStartData.self.name} of ${rtmStartData.team.name}`.blue)
 
-    users = rtmStartData.users
+    users = rtmStartData.users.filter(user => !user.is_restricted).filter(user => !user.is_bot).filter(user => !!user.real_name)
 
-    let activeUsers = rtmStartData.users.filter(user => !user.is_bot).filter(user => user.presence === STATES.ACTIVE)
-    let awayUsers = rtmStartData.users.filter(user => !user.is_bot).filter(user => user.presence === STATES.AWAY)
+    let activeUsers = users.filter(user => !user.is_bot).filter(user => user.presence === STATES.ACTIVE)
+    let awayUsers = users.filter(user => !user.is_bot).filter(user => user.presence === STATES.AWAY)
 
-    console.log('%s users active and %s users away'.yellow, activeUsers.length, awayUsers.length)
+    log.log(`${activeUsers.length} users active and ${awayUsers.length} users away`.yellow)
     if (activeUsers.length) {
-        console.log('Users online: %s'.yellow, activeUsers.map(user => user.real_name).join(', '))
+        log.log(`Users online: ${activeUsers.map(user => user.real_name).join(', ')}`.yellow)
     }
+
+    onlineUsers.content = users.map((user) => {
+        if (user.presence === STATES.ACTIVE) {
+            return `•  ${[user.real_name]}`.green
+        }
+    }).filter(message => !!message).join('\n')
+
+    offlineUsers.content = users.map((user) => {
+        if (user.presence !== STATES.ACTIVE) {
+            return `◦  ${[user.real_name]}`.yellow
+        }
+    }).filter(message => !!message).join('\n')
 })
 
 slack.on(RTM_EVENTS.PRESENCE_CHANGE, function (changedUser) {
@@ -28,10 +40,40 @@ slack.on(RTM_EVENTS.PRESENCE_CHANGE, function (changedUser) {
     fullUserDetails.presence = changedUser.presence
 
     if (changedUser.presence === STATES.AWAY) {
-        console.log('%s has gone %s'.red, fullUserDetails.real_name, changedUser.presence)
+        log.log(`${fullUserDetails.real_name} has gone ${changedUser.presence}`.red)
     } else {
-        console.log('%s has become %s'.green, fullUserDetails.real_name, changedUser.presence)
+        log.log(`${fullUserDetails.real_name} has become ${changedUser.presence}`.green)
     }
+
+    onlineUsers.content = users.map((user) => {
+        if (user.presence === STATES.ACTIVE) {
+            return `•  ${[user.real_name]}`.green
+        }
+    }).filter(message => !!message).join('\n')
+
+    offlineUsers.content = users.map((user) => {
+        if (user.presence !== STATES.ACTIVE) {
+            return `◦  ${[user.real_name]}`.yellow
+        }
+    }).filter(message => !!message).join('\n')
 })
 
 slack.login()
+
+var blessed = require('blessed')
+var contrib = require('blessed-contrib')
+
+var screen = blessed.screen()
+
+var grid = new contrib.grid({ rows: 12, cols: 12, screen: screen })
+
+//grid.set(row, col, rowSpan, colSpan, obj, opts)
+var log = grid.set(0, 0, 12, 6, contrib.log, { label: 'Activity Log' })
+var onlineUsers = grid.set(0, 6, 12, 3, blessed.box, { label: 'Online Users', content: '' })
+var offlineUsers = grid.set(0, 9, 12, 3, blessed.box, { label: 'Offline Users', content: '' })
+
+screen.key(['escape', 'q', 'C-c'], function (ch, key) {
+    return process.exit(0);
+});
+
+screen.render()
